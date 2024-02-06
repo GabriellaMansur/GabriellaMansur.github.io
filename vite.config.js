2bb433bad682ccc53c4c689b05c8e0a7dae32cdb
Gabriella Mansur <gabriellamesquita314@gmail.com>
2024-02-06 11:43:58 -0300

    config de deplo2y

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./portifolio",
+  base: "./GabriellaMansur.github.io",
   css: {
     modules: {
       localsConvention: "camelCase",
